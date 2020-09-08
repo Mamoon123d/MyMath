3a176d3ee8d77644ca1f73edbee34915072adee9
mamoon123 <moonasus558@gmail.com>
2020-09-08 21:33:13 +0530

    commit
    custom view

---
+++ MyView/src/main/java/com/moon/myview/MyView.java
@@ -0,0 +1,67 @@
+package com.moon.myview;
+
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Rect;
+import android.util.AttributeSet;
+import android.view.View;
+
+import androidx.annotation.Nullable;
+
+class MyView extends View {
+    private static final int shape_size=200;
+    private Rect mRectSquare;
+    private Paint mPaintSquare;
+    private int mShapeColor;
+    private int mShapeSize;
+    public MyView(Context context) {
+        super(context);
+        init(null);
+    }
+
+    public MyView(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+        init(null);
+
+    }
+
+    public MyView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        init(null);
+
+    }
+
+    public MyView(Context context, @Nullable AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+        super(context, attrs, defStyleAttr, defStyleRes);
+        init(null);
+
+    }
+
+    private void init(AttributeSet set) {
+        mRectSquare=new Rect();
+        mPaintSquare=new Paint(Paint.ANTI_ALIAS_FLAG);
+        mPaintSquare.setColor(Color.RED);
+
+        if (set==null)
+            return;
+
+        TypedArray typedArray=getContext().obtainStyledAttributes(set,R.styleable.MyView);
+        mShapeColor=typedArray.getColor(R.styleable.MyView_shape_color,Color.RED);
+        mShapeSize=typedArray.getColor(R.styleable.MyView_shape_size,shape_size);
+
+        typedArray.recycle();
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        mRectSquare.left=50;
+        mRectSquare.top=50;
+        mRectSquare.right=mRectSquare.left+mShapeSize;
+        mRectSquare.bottom=mRectSquare.top+mShapeSize;
+        canvas.drawRect(mRectSquare,mPaintSquare);
+
+    }
+}
--- app/src/main/java/com/moon/mymaths/MainActivity.java
+++ app/src/main/java/com/moon/mymaths/MainActivity.java
@@ -1,14 +1,20 @@
 package com.moon.mymaths;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.moon.myview.myCustomView;
+
+public class MainActivity extends AppCompatActivity  {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        FragmentManager fragmentManager=getSupportFragmentManager();
+        myCustomView MyView=(myCustomView)fragmentManager.findFragmentById(R.id.fragment);
+        MyView.changeText("hi");
     }
 }
